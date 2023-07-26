bb0df88150e9e528a559d709f0622fc4d8a39207
Proctor <ntproctor@wpi.edu>
2023-07-26 15:07:36 -0400

    wed 3:00

--- Num Game/numgame.js
+++ Num Game/numgame.js
@@ -1,5 +1,7 @@
 const guess = document.getElementById("guess");
 const report = document.getElementById("report");
+const body = document.body;
+let start = Date.now(); // remember start time
 
 const MAXNUM = 100;
 let secret;
@@ -7,9 +9,25 @@ let secret;
 function loadGame() {
     guess.max = MAXNUM;
     secret = Math.floor(Math.random() * (MAXNUM+1));
-    report.innerHTML = secret;
+    // report.innerHTML = secret;
 }
 
 function makeGuess() {
-
+    let myGuess =  guess.value;
+    if (myGuess<secret){
+        report.innerHTML +=`<br/>{${myGuess}] too small`
+        // console.log(`myGuess=${myGuess}`);
+        
+        
+    } else if (myGuess > secret){
+        report.innerHTML +=`<br/>{${myGuess}] too large`
+        
+    }else {
+        report.innerHTML +=`<br/>{${myGuess}] is correct!`
+        // body.style.backgroundColor=`#ddddff`
+       
+        
+        
+    }
+    
 }
